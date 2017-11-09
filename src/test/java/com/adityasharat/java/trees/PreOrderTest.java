a83e528de4a6907f6ab8eb81943fce1bedec18ae
Aditya Sharat <adityasharat@gmail.com>
2017-11-09 15:04:50 +0530

    changes

---
+++ src/test/java/com/adityasharat/java/trees/PreOrderTest.java
@@ -0,0 +1,36 @@
+package com.adityasharat.java.trees;
+
+import com.adityasharat.java.Functions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+/**
+ * @author Aditya Sharat
+ */
+public class PreOrderTest {
+
+    @Test
+    public void run() {
+
+        Node<String> _1 = new Node<>("1");
+        Node<String> _2 = new Node<>("2");
+        Node<String> _3 = new Node<>("3");
+        Node<String> _4 = new Node<>("4");
+        Node<String> _5 = new Node<>("5");
+
+        _1.left = _2;
+        _1.right = _3;
+
+        _2.left = _4;
+        _2.right = _5;
+
+        Node<String> root = _1;
+
+        List<String> result = Functions.preOrder(root);
+        StringJoiner joiner = new StringJoiner("");
+        result.forEach(joiner::add);
+        System.out.println(result);
+    }
+}

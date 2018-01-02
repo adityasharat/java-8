27fd415aaf7c5dae4eb777af9972938b2c4c2192
Aditya Sharat <adityasharat@gmail.com>
2018-01-02 13:58:07 +0530

    add new module

---
+++ lesson-1/src/main/java/com/adityasharat/java/lesson1/Main.java
@@ -0,0 +1,11 @@
+package com.adityasharat.java.lesson1;
+
+/**
+ * @author Aditya Sharat
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

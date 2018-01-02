d20b61387ca23b3fd4b47b71cf6382716ca12d33
Aditya Sharat <adityasharat@gmail.com>
2018-01-02 14:36:40 +0530

    primitives

---
+++ lesson-1/src/main/java/com/adityasharat/java/lesson1/Primitives.java
@@ -0,0 +1,43 @@
+package com.adityasharat.java.lesson1;
+
+/**
+ * @author Aditya Sharat
+ */
+public class Primitives {
+
+    public static void main(String[] args) {
+
+        /* integer */
+
+        int x;  // declaration of a variable.
+
+        x = 0;  // assigning a value to a variable.
+
+        int y = 100; // declaring and assigning a value.
+
+        int a = -1, b = 2; // declaring and assigning a value of multiple variables in the same line
+
+        /* double */
+        double c = 1.5;
+        double d = -1.6174;
+
+        /* float */
+        float e = 1.75f;
+        float f = -0.276f;
+        float g = 2f;
+
+        float h = g;    // assigning value from another variable
+
+        /* long */
+        long i = 1234567891;
+
+        /* booleans */
+        boolean imABoolean = true;
+        imABoolean = false;
+
+        char j = 'j';
+
+        byte k = 127;
+        short l = k;
+    }
+}

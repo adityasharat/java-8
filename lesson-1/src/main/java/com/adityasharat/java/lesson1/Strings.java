832f5a4c86e9ea2a9d31ec2eff697a657968e3e8
Aditya Sharat <adityasharat@gmail.com>
2018-01-08 15:21:20 +0530

    Strings

--- lesson-1/src/main/java/com/adityasharat/java/lesson1/Strings.java
+++ lesson-1/src/main/java/com/adityasharat/java/lesson1/Strings.java
@@ -8,19 +8,29 @@ import java.util.Objects;
 public class Strings {
 
     public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+
+        if (x == y) {
+            System.out.println("x == y");
+        } else {
+            System.out.println("x != y");
+        }
+
+
         String hello1 = "Hello !!";
         String hello2 = "Hello !!";
 
-        if (hello1 == hello2) {
-            System.out.println("== : They are equal");
+        if (hello1 == hello2) { // == does not compare non primitive types (expect Wrappers)
+            System.out.println("hello1 == hello2");
         } else {
-            System.out.println("== : They are not equal");
+            System.out.println("hello1 != hello2");
         }
 
         if (Objects.equals(hello1, hello2)) {
-            System.out.println(".equals : They are equal");
+            System.out.println("hello1 equals hello2");
         } else {
-            System.out.println(".equals : They are not equal");
+            System.out.println("hello1 not equals hello2");
         }
 
     }

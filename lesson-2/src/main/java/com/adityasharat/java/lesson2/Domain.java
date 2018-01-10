a83a4d36ecd3ae6ebc26a59e8cfba63ee0ecad0a
Aditya Sharat <adityasharat@gmail.com>
2018-01-10 16:48:42 +0530

    add Domain

---
+++ lesson-2/src/main/java/com/adityasharat/java/lesson2/Domain.java
@@ -0,0 +1,16 @@
+package com.adityasharat.java.lesson2;
+
+import com.sun.istack.internal.NotNull;
+
+/**
+ * @author Aditya Sharat
+ */
+public class Domain {
+
+    @NotNull
+    public final String name;
+
+    public Domain(String name) {
+        this.name = name;
+    }
+}

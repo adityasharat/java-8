5927c11b0894c50b148b8736040646730baa6972
Aditya Sharat <adityasharat@gmail.com>
2018-01-11 15:25:13 +0530

    add entity.

--- lesson-2/src/main/java/com/adityasharat/java/lesson2/life/Domain.java
+++ lesson-2/src/main/java/com/adityasharat/java/lesson2/life/Domain.java
@@ -1,16 +1,21 @@
 package com.adityasharat.java.lesson2.life;
 
+import com.adityasharat.java.lesson2.Entity;
 import com.sun.istack.internal.NotNull;
 
 /**
  * @author Aditya Sharat
  */
-public class Domain {
+public class Domain implements Entity {
 
     @NotNull
-    public final String name;
+    private final String name;
 
     public Domain(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }

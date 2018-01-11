a1e9b3c8d995d7e2ff1feebe625cb5bee1f9c6cf
Aditya Sharat <adityasharat@gmail.com>
2018-01-11 15:07:12 +0530

    refactor

---
+++ lesson-2/src/main/java/com/adityasharat/java/lesson2/Entity.java
@@ -0,0 +1,13 @@
+package com.adityasharat.java.lesson2;
+
+import com.sun.istack.internal.NotNull;
+
+/**
+ * @author Aditya Sharat
+ */
+public interface Entity {
+
+    @NotNull
+    String getName();
+
+}
